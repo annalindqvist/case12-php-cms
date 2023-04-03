0ec4a5370ce2ff52b63d5ef2715e3f139bc25dfa
annalindqvist <annalindqvist_@live.se>
2023-04-03 13:31:47 +0200

    session msg to include

--- app/public/cms-admin/signin.php
+++ app/public/cms-admin/signin.php
@@ -64,33 +64,7 @@ $title = "Login";
 </head>
 
 <body class="w-screen flex">
-    <?php 
-    // Session message
-    if (isset($_SESSION['message']) && !empty($_SESSION['message'])) {
-        echo "<div id='session-div' class='fixed top-10 inset-x-0 space-x-2'>
-        <div
-          class='pointer-events-auto mx-auto hidden w-96 max-w-full rounded-lg bg-white bg-clip-padding text-sm shadow-lg shadow-black/5 data-[te-toast-show]:block data-[te-toast-hide]:hidden dark:bg-neutral-600'
-          id='static-example' role='alert' aria-live='assertive' aria-atomic='true' data-te-autohide='false' data-te-toast-init data-te-toast-show>
-          <div class='flex items-center justify-between rounded-t-lg border-b-2 border-blue-500 border-opacity-100 bg-white bg-clip-padding px-4 pt-2.5 pb-2 dark:border-opacity-50 dark:bg-neutral-600'>
-            <p class='font-bold text-neutral-500 dark:text-neutral-200'>Message</p>
-            <div class='flex items-center'><button id='session-msg-btn' type='button'
-                class='ml-2 box-content rounded-none border-none opacity-80 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none'
-                data-te-toast-dismiss aria-label='Close'> <span class='w-[1em] focus:opacity-100 disabled:pointer-events-none disabled:select-none disabled:opacity-25 [&.disabled]:pointer-events-none [&.disabled]:select-none [&.disabled]:opacity-25'>
-                  <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'
-                    stroke-width='1.5'
-                    stroke='currentColor'
-                    class='h-6 w-6'> <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      d='M6 18L18 6M6 6l12 12' />
-                  </svg></span></button></div></div> <div class='break-words rounded-b-lg bg-white py-4 px-4 text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200'>"
-                  . $_SESSION['message'] .
-          "</div>
-        </div>
-      </div>";
-        unset( $_SESSION['message']);
-    }
-    ?>
+<?php include ROOT . '/cms-includes/partials/session_msg.php'; ?>
 
     
 
